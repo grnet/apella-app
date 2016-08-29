0b73cd1633ead1e0f33f631a29b25b7c49c7f1f6
Olga Brani <olgabrani78@gmail.com>
2016-08-29 12:03:00 +0300

    Add role option to user-add command

--- apella/management/commands/user-add.py
+++ apella/management/commands/user-add.py
@@ -1,17 +1,33 @@
+from optparse import make_option
+
 from django.core.management.base import BaseCommand, CommandError
 from apella.models import ApellaUser
+
 
 class Command(BaseCommand):
     help = 'Creates a user'
     args = '<username password>'
 
+    option_list = BaseCommand.option_list + (
+        make_option('--role',
+                    dest='role',
+                    default=2,
+                    choices=[1, 2, 3, 4],
+                    help='Choose a role for the user'),
+        )
+
     def handle(self, *args, **options):
         if len(args) != 2:
             raise CommandError("Invalid number of arguments")
         username, password = args[:2]
+
         try:
-            a= ApellaUser(username=username,password=password)
-            a.save()
+            a = ApellaUser.objects.create(username=username,
+                                          password=password,
+                                          role=options['role'])
+
             self.stdout.write("User with id: %s created" % a.pk)
+        except BaseException, e:
+            raise CommandError(e)
         except:
             self.stdout.write("Operation failed")
