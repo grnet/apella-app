69596fe97e73276db4ff2ce8d5a09f16459ee25d
Olga Brani <olgabrani78@gmail.com>
2016-08-30 12:00:43 +0300

    Add set-committee/set-electors to position-modify

--- apella/management/commands/position-modify.py
+++ apella/management/commands/position-modify.py
@@ -11,14 +11,23 @@ class Command(BaseCommand):
     option_list = BaseCommand.option_list + (
         make_option('--set-elected',
                     dest='elected',
-                    help='Choose an elected for the position'),
+                    help='Choose an elected user for the position'),
+        make_option('--set-electors',
+                    dest='electors',
+                    help='Choose electors for the position'),
+        make_option('--set-committee',
+                    dest='committee',
+                    help='Choose committee for the position'),
         )
+
 
     def handle(self, *args, **options):
         if len(args) != 1:
             raise CommandError("Please provide a position ID")
         position_id = args[0]
         elected = options['elected']
+        electors = options['electors']
+        committee = options['committee']
 
         if position_id.isdigit():
             try:
@@ -34,6 +43,9 @@ class Command(BaseCommand):
                     elected = ApellaUser.objects.get(id=int(elected))
                     position.elected = elected
                     position.save()
+                    self.stdout.write("%s has been elected for position %s" %
+                            (elected.username, position.id))
+
                 except ApellaUser.DoesNotExist:
                     raise CommandError("Invalid elected ID")
                 except:
@@ -42,5 +54,51 @@ class Command(BaseCommand):
             else:
                 raise CommandError("Elected ID must be an integer")
 
+        if electors:
+            try:
+                electors_ids = electors.split(',')
+                for e in electors_ids:
+                    if e.isdigit():
+                        try:
+                            u = ApellaUser.objects.get(id=int(e))
+                            position.electors.add(u)
+                            position.save()
+                            self.stdout.write(
+                                    "%s is an elector for position %s" %
+                                    (u.username, position.id))
+
+                        except ApellaUser.DoesNotExist:
+                            raise CommandError("Invalid elector ID %s" % e)
+                        except:
+                            raise CommandError("Operation failed")
+                    else:
+                        raise CommandError(
+                                "Elector ID %s  must be an integer" % e)
+
+            except TypeError:
+                    print 'Electors should be comma separated user ids'
 
 
+        if committee:
+            try:
+                committee_ids = committee.split(',')
+                for c in committee_ids:
+                    if c.isdigit():
+                        try:
+                            u = ApellaUser.objects.get(id=int(c))
+                            position.committee.add(u)
+                            position.save()
+                            self.stdout.write(
+                                    "%s is in committee for position %s" %
+                                    (u.username, position.id))
+
+                        except ApellaUser.DoesNotExist:
+                            raise CommandError("Invalid elector ID %s" % c)
+                        except:
+                            raise CommandError("Operation failed")
+                    else:
+                        raise CommandError(
+                                "Elector ID %s  must be an integer" % c)
+
+            except TypeError:
+                    print 'Committee should be comma separated user ids'
