56fd72d99db8135d2a2320d3cd1b3480289eec7e
Efthymia Bika <ey8ymia@grnet.gr>
2016-09-06 14:38:51 +0300

    Add setup.py
    Management commands are now available everywhere
    * python setup.py install
    * apella <command>

---
+++ setup.py
@@ -0,0 +1,55 @@
+from setuptools import setup, find_packages
+
+# Package info
+PACKAGE_NAME = "apella"
+VERSION = "0.1"
+SHORT_DESCRIPTION = "Election of Faculty members"
+
+PACKAGES_ROOT = '.'
+PACKAGES = find_packages(PACKAGES_ROOT)
+
+# Package meta
+CLASSIFIERS = []
+
+# Package requirements
+with open('requirements.txt') as reqs:
+    install_requires = [
+        line for line in reqs.read().split('\n') if (line and not
+                                                     line.startswith('--'))
+    ]
+
+EXTRAS_REQUIRES = {
+}
+
+TESTS_REQUIRES = [
+]
+
+# Provided as an attribute, so you can append to these instead
+# of replicating them:
+standard_exclude = ["*.py", "*.pyc", "*$py.class", "*~", ".*", "*.bak"]
+standard_exclude_directories = [
+    ".*", "CVS", "_darcs", "./build", "./dist", "EGG-INFO", "*.egg-info",
+]
+
+setup(
+    name=PACKAGE_NAME,
+    version=VERSION,
+    license='GNU GPLv3',
+    description=SHORT_DESCRIPTION,
+    classifiers=CLASSIFIERS,
+
+    packages=PACKAGES,
+    package_dir={'': PACKAGES_ROOT},
+    include_package_data=True,
+    zip_safe=False,
+
+    install_requires=install_requires,
+    extras_require=EXTRAS_REQUIRES,
+    tests_require=TESTS_REQUIRES,
+
+    entry_points={
+        'console_scripts': [
+            'apella = apella.management:main',
+        ],
+    },
+)
