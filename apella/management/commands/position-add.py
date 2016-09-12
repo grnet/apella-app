4f72a8731438e3305dcbd444f0225d371b1209fd
Efthymia Bika <ey8ymia@grnet.gr>
2016-09-12 12:18:49 +0300

    Fix position-add command to include extra fields

--- apella/management/commands/position-add.py
+++ apella/management/commands/position-add.py
@@ -1,6 +1,6 @@
 from django.core.management.base import BaseCommand, CommandError
 from django.core.exceptions import ValidationError
-from apella.models import ApellaUser, Position, Department
+from apella.models import ApellaUser, Position, Department, Subject
 from apella.management.utils import get_user
 
 from optparse import make_option
@@ -8,7 +8,8 @@ from optparse import make_option
 
 class Command(BaseCommand):
     help = 'Create a position with the given title and author'
-    args = '<title> <author id or username> <department id>'
+    args = '<title> <author id or username> <department id> <description>' + \
+        ' <subject> <fek url> <fek posted at>'
 
     option_list = BaseCommand.option_list + (
         make_option('--start',
@@ -22,10 +23,11 @@ class Command(BaseCommand):
                         )
 
     def handle(self, *args, **options):
-        if len(args) != 3:
+        if len(args) != 7:
             raise CommandError("Invalid number of arguments")
 
-        title, author, department_id = args[:3]
+        title, author, department_id, description, \
+            subject_id, fek, fek_posted_at = args[:7]
         # TODO: validate dates
         starts_at = options['starts_at']
         ends_at = options['ends_at']
@@ -33,10 +35,14 @@ class Command(BaseCommand):
         try:
             position_author = get_user(author)
             department = Department.objects.get(id=department_id)
+            subject = Subject.objects.get(id=subject_id)
+
             p = Position.objects.create(
                     title=title, author=position_author,
-                    department=department,
-                    starts_at=starts_at, ends_at=ends_at)
+                    department=department, description=description,
+                    fek=fek, fek_posted_at=fek_posted_at,
+                    subject=subject, starts_at=starts_at, ends_at=ends_at)
+
             self.stdout.write(
                 "Created position %s : title = %s author = %s" %
                 (p.pk, p.title, p.author.username))
@@ -46,3 +52,5 @@ class Command(BaseCommand):
             raise CommandError("User %s does not exist" % author)
         except Department.DoesNotExist:
             raise CommandError("Department %s does not exist" % department_id)
+        except Subject.DoesNotExist:
+            raise CommandError("Subject %s does not exist" % subject_id)
