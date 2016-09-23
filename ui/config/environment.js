49731d410dab4f7404c94ce2de1e56ae992dd1c8
Olga Brani <olgabrani78@gmail.com>
2016-09-23 14:28:30 +0300

    Set 'ui' as baseURL for Ember

--- ui/config/environment.js
+++ ui/config/environment.js
@@ -4,7 +4,7 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'ui',
     environment: environment,
-    rootURL: '/',
+    rootURL: '/ui/',
     locationType: 'auto',
     EmberENV: {
       FEATURES: {
