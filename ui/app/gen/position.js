74675e2bfb630f59ef2a4068cca3cad75fa04fdb
Olga Brani <olgabrani78@gmail.com>
2016-10-03 17:24:22 +0300

    ui: Minor changes to position form

--- ui/app/gen/position.js
+++ ui/app/gen/position.js
@@ -11,26 +11,26 @@ export default gen.CRUDGen.extend({
     validators: {
       title: [validate.presence(true), validate.length({min:4, max:50})],
       description: [validate.presence(true), validate.length({max:300})],
+      fek: [validate.format({type: 'url'})]
     },
     fieldsets: [{
       label: 'fieldsets.labels.basic_info',
-      fields: ['title', 'state', 'description', 'discipline', 'department', 'subject_area', 'subject'],
+      fields: ['title',
+              ['state', {
+                attrs: {
+                  readonly: true,
+                }
+              }],
+              'description', 'discipline', 'department', 'subject_area', 'subject'],
       layout: {
         flex: [50, 50, 100, 50, 50, 50, 50]
       }
     }, {
-      label: 'position.fek_section.title',
-      fields: ['fek', 'fek_posted_at'],
-      layout: {
-        flex: [50, 50]
-      },
-    }, {
       label: 'fieldsets.labels.details',
-      fields: ['electors', 'committee', 'assistants', 'elected', 'starts_at', 'ends_at'],
+      fields: ['fek', 'fek_posted_at', 'starts_at', 'ends_at'],
       layout: {
-        flex: [50, 50, 50, 50, 50, 50]
-      }
-
+        flex: [50, 50, 50, 50]
+      },
     }],
   }
 });
