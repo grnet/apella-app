8534f3fceb9a26d01c3ee3dceca7dedc0a215ed6
Olga Brani <olgabrani78@gmail.com>
2016-10-04 16:28:37 +0300

    ui: Add basic language-select component

---
+++ ui/app/components/language-select.js
@@ -0,0 +1,22 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  i18n: Ember.inject.service(),
+  classNames: ['language-select'],
+
+  locales: Ember.computed('i18n.locale', 'i18n.locales', function() {
+    const i18n = this.get('i18n');
+    return this.get('i18n.locales').map(function (loc) {
+      return { id: loc, text: i18n.t('language-select.language.' + loc) };
+    });
+  }),
+
+  actions: {
+    setLocale(e) {
+      console.log(e, '000000000');
+      console.log(this.get('loc'));
+      debugger;
+      this.set('i18n.locale', this.$('select').val());
+    }
+  }
+});
