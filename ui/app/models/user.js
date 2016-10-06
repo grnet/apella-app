02db314165269b36b6cb3b5f42249ab52686a3ef
Olga Brani <olgabrani78@gmail.com>
2016-10-06 17:03:59 +0300

    ui: Add verbose role to user model

--- ui/app/models/user.js
+++ ui/app/models/user.js
@@ -6,5 +6,9 @@ const CHOICES = ENV.APP.resource_choices;
 export default DS.Model.extend({
   username: DS.attr(),
   role: DS.attr({type: 'select', choices: CHOICES.USER_ROLES}),
+  role_verbose: Ember.computed('role', function(){
+    let role_id = `${this.get('role')}`;
+    return (CHOICES.USER_ROLES.find(x => x[0] == role_id) || [0, ''])[1]
+  }),
 
 });
