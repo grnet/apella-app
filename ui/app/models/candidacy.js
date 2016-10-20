c85a38727020c232308bb15f0e085839d2dffc2c
Athina Bekakou <athinabek@grnet.gr>
2016-10-20 18:36:33 +0300

    ui: Add boolean type props in candidacy

--- ui/app/models/candidacy.js
+++ ui/app/models/candidacy.js
@@ -10,7 +10,7 @@ export default DS.Model.extend({
   candidate: DS.belongsTo('user', {label: 'candidacy.label.candidate', attrs: {type: 'select', optionLabelAttr: 'username'}}),
   position: DS.belongsTo('position', {label: 'candidacy.label.position', attrs: {type: 'select', optionLabelAttr: 'id_and_title'}}),
   state: DS.attr({label: 'candidacy.label.state'}),
-  othersCanView: DS.attr({label: 'candidacy.label.others_can_view'}),
+  othersCanView: DS.attr({type: 'boolean', label: 'candidacy.label.others_can_view'}),
   submittedAt: DS.attr('date'),
   cv: DS.attr({label: 'candidacy.label.cv'}),
   diploma: DS.attr({label: 'candidacy.label.diploma'}),
