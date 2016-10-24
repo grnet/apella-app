80e144091c48282aeb67cd5c172c35035c468351
Olga Brani <olgabrani78@gmail.com>
2016-10-24 11:03:21 +0300

    ui: Enable some candidacy fields for posted position
    When position state is "Posted", the candidate can modify self evalution
    report, complementary documents and visibility to others.
    For other position states, these fields are readonly.

--- ui/app/gen/candidacy.js
+++ ui/app/gen/candidacy.js
@@ -1,6 +1,7 @@
 import {field} from 'ember-gen';
 import gen from 'ember-gen/lib/gen';
 import validate from 'ember-gen/validate';
+import _ from 'lodash/lodash'
 
 const presence = validate.presence(true),
       max_chars = validate.length({max: 200}),
@@ -9,7 +10,8 @@ const presence = validate.presence(true),
       {
         get, computed
       } =Ember,
-      CANDIDACY_POSTED_ID = '2';
+      CANDIDACY_POSTED_ID = '2',
+      POSITION_POSTED_ID = '2';
 
 let FS = {
   list:  ['position.code', 'position.department.school.institution.title_current',
@@ -19,36 +21,6 @@ let FS = {
     label: 'candidacy.position_section.title',
     text: 'candidacy.position_section.subtitle',
     fields: ['position'],
-    layout: {
-      flex: [50]
-    },
-  },
-  {
-    label: 'candidacy.candidate_section.title',
-    text: 'candidacy.candidate_section.subtitle',
-    fields: ['candidate', 'cv', 'diploma', 'publication'],
-    layout: {
-      flex: [50, 50, 50, 50]
-    },
-  },
-    {
-      label: 'candidacy.candidacy_section.title',
-      fields: ['selfEvaluation', 'additionalFiles', 'othersCanView'],
-      layout: {
-        flex: [50, 50, 50, 50]
-      }
-    }
-  ],
-  edit: [{
-    label: 'candidacy.position_section.title',
-    text: 'candidacy.position_section.subtitle',
-    fields: [field('position', {
-        hint: 'candidacy.edit.position.hint',
-        attrs: {
-          optionLabelAttr: 'code_and_title',
-          readonly: true,
-        }
-    })],
     layout: {
       flex: [50]
     },
@@ -148,6 +120,49 @@ export default gen.CRUDGen.extend({
       label: 'common.button.edit_label',
       icon: 'border color'
     },
-    fieldsets: FS.edit,
+    fieldsets: computed('model.position.state', function() {
+      let candidacy_fields = ['selfEvaluation', 'additionalFiles', 'othersCanView'];
+      let readonly_field = function(el){
+        return field(el, {
+          attrs: {
+            readonly: true
+          }
+        })
+      }
+      if (this.get('model.position.state') != POSITION_POSTED_ID) {
+        candidacy_fields = _.map(candidacy_fields, readonly_field);
+      };
+
+      return [{
+        label: 'candidacy.position_section.title',
+        text: 'candidacy.position_section.subtitle',
+        fields: [field('position', {
+            hint: 'candidacy.edit.position.hint',
+            attrs: {
+              optionLabelAttr: 'code_and_title',
+              readonly: true,
+            }
+        })],
+        layout: {
+          flex: [50]
+        },
+      },
+      {
+        label: 'candidacy.candidate_section.title',
+        text: 'candidacy.candidate_section.subtitle',
+        fields: ['candidate', 'cv', 'diploma', 'publication'],
+        layout: {
+          flex: [50, 50, 50, 50]
+        },
+      },
+        {
+          label: 'candidacy.candidacy_section.title',
+          fields: candidacy_fields,
+          layout: {
+            flex: [50, 50, 50, 50]
+          }
+        }
+      ];
+    }),
   },
 });
