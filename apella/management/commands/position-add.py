d5f2afc9f686c3551e5a5c3df8cb9991d5ed3984
Efthymia Bika <ey8ymia@grnet.gr>
2016-10-27 11:15:24 +0300

    Update position-add command

--- apella/management/commands/position-add.py
+++ apella/management/commands/position-add.py
@@ -1,24 +1,39 @@
 from django.core.management.base import BaseCommand, CommandError
 from django.core.exceptions import ValidationError
+
 from apella.models import Position, Department, Subject, SubjectArea,\
     InstitutionManager
-from apella.management.utils import get_user, ApellaCommand
-
-from optparse import make_option
+from apella.management.utils import ApellaCommand
 
 
 class Command(ApellaCommand):
-    help = 'Create a position with the given title and author'
-    args = '<title> <author id> <department id> <description>' + \
-        ' <subject area> <subject> <fek url> <fek posted at> <start> <end>'
+    help = 'Create a position'
+
+    def add_arguments(self, parser):
+        parser.add_argument('title')
+        parser.add_argument('author')
+        parser.add_argument('department_id')
+        parser.add_argument('description')
+        parser.add_argument('subject_area_id')
+        parser.add_argument('subject_id')
+        parser.add_argument('fek')
+        parser.add_argument('fek_posted_at')
+        parser.add_argument('starts_at')
+        parser.add_argument('ends_at')
+        parser.add_argument('discipline')
 
     def handle(self, *args, **options):
-        if len(args) != 10:
-            raise CommandError("Invalid number of arguments")
-
-        title, author, department_id, description,\
-            subject_area_id, subject_id, fek, fek_posted_at,\
-            starts_at, ends_at = args[:10]
+        title = options['title']
+        author = options['author']
+        department_id = options['department_id']
+        description = options['description']
+        discipline = options['discipline']
+        subject_area_id = options['subject_area_id']
+        subject_id = options['subject_id']
+        fek = options['fek']
+        fek_posted_at = options['fek_posted_at']
+        starts_at = options['starts_at']
+        ends_at = options['ends_at']
 
         try:
             position_author = InstitutionManager.objects.get(id=author)
@@ -30,7 +45,7 @@ class Command(ApellaCommand):
                     title=title, author=position_author,
                     department=department, description=description,
                     fek=fek, fek_posted_at=fek_posted_at,
-                    subject_area=subject_area,
+                    subject_area=subject_area, discipline=discipline,
                     subject=subject, starts_at=starts_at, ends_at=ends_at)
 
             self.stdout.write(
