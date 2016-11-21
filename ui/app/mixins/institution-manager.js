f282648df6f412a3dd73bb742a0c6e84b74a2dcb
Olga Brani <olgabrani78@gmail.com>
2016-11-21 13:06:05 +0200

    ui: Change attrs to formAtts for mixins

--- ui/app/mixins/institution-manager.js
+++ ui/app/mixins/institution-manager.js
@@ -5,7 +5,7 @@ import ENV from 'ui/config/environment';
 const CHOICES = ENV.APP.resource_choices;
 
 export default Ember.Mixin.create({
-  institution: DS.belongsTo('institution', {label: 'institution.label', attrs: {optionLabelAttr: 'title_current'}}),
+  institution: DS.belongsTo('institution', {label: 'institution.label', formAttrs: {optionLabelAttr: 'title_current'}}),
   authority: DS.attr({label: 'manager.label.authority_type', type: 'select', choices: CHOICES.AUTHORITIES}),
   authority_full_name: DS.attr({label: 'manager.label.authority_full_name'}),
   manager_role: DS.attr({label: 'manager.label.role', type: 'select', choices: CHOICES.MANAGER_ROLES})
