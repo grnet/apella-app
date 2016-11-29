524dc527012904802037d07872c58f82f3aeb46e
Athina Bekakou <athinabek@grnet.gr>
2016-11-29 17:12:50 +0200

    ui: Update subject model

--- ui/app/models/subject.js
+++ ui/app/models/subject.js
@@ -3,7 +3,7 @@ import gen from 'ember-gen/lib/attrs';
 import titleI18NMixin from 'ui/mixins/title-current';
 
 export default DS.Model.extend(titleI18NMixin, {
-  title: DS.attr({component: 'i18n-input-field'}),
+  title: DS.attr({formComponent: 'i18n-input-field'}),
   area: gen.belongsTo('subject_area', {formAttrs: {optionLabelAttr: 'title_current'}}),
 
 
