26e86d0335cff2e7252b3c2d14bfcb3fd7cef050
Kostas Papadimitriou <kpap@grnet.gr>
2016-11-30 12:18:48 +0200

    application.index beforeModel hook to redirect in accordance with user role

---
+++ ui/app/routes/index.js
@@ -0,0 +1,23 @@
+import Ember from 'ember';
+
+const {
+  get
+} = Ember;
+
+const ROLE_ROUTE_MAP = {
+  'institutionmanager': 'institution.index',
+  'professor': 'position.index',
+}
+
+const DEFAULT_ROUTE = 'auth.profile';
+
+export default Ember.Route.extend({
+  session: Ember.inject.service(),
+  beforeModel(transition) {
+    if (get(this, 'session.isAuthenticated')) {
+      let role = get(this, 'session.session.authenticated.role');
+      transition.abort();
+      return this.transitionTo(ROLE_ROUTE_MAP[role] || DEFAULT_ROUTE);
+    }
+  }
+});
