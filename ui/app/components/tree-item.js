ba409f98e7e5e59f4c2fdbb4796477d8d9b31a61
Olga Brani <olgabrani78@gmail.com>
2016-12-05 16:45:34 +0200

    ui: Observe changes in nodes/root in user interest

--- ui/app/components/tree-item.js
+++ ui/app/components/tree-item.js
@@ -7,6 +7,7 @@ const {
   get,
   set,
   isArray,
+  observer,
   computed,
   computed: { alias, equal, gt, map }
 } = Ember;
@@ -19,8 +20,57 @@ export default Ember.Component.extend({
   store: Ember.inject.service(),
   expanded: false,
 
+  rootIsSelected: computed('item', 'selected.[]', function(){
+    return get(this, 'selected').includes(get(this, 'item'));
+  }),
+
   toggleIcon: computed('expanded', function(){
     return this.get('expanded')? 'expand-more': 'chevron-right'
+  }),
+
+  observeRoot: observer('rootIsSelected', 'expanded', function(){
+    let expanded = get(this, 'expanded');
+    let subModelType = get(this, 'subModelType');
+    let rootIsSelected = get(this, 'rootIsSelected');
+    if (expanded && rootIsSelected ) {
+      let nodes = get(this, 'nodes');
+      nodes.then( () => {
+        nodes.forEach( (el) => {
+          this.sendAction('onChange', false, el, subModelType);
+        })
+      })
+    }
+  }),
+
+  observeNodes: observer('subSelected.[]', 'expanded', 'item',  function(){
+    let subSelected = get(this, 'subSelected');
+    let expanded = get(this, 'expanded');
+    let type = get(this, 'modelType');
+    if (type=='subject-area') { type = 'area' }
+    let el = get(this, 'item');
+    let self = this;
+
+    if (expanded) {
+      let nodes = get(this, 'nodes');
+      nodes.then( () => {
+        let nodesNum = get(nodes, 'length');
+        if (nodesNum>0) {
+          let cnt = 0;
+          nodes.forEach( (el) => {
+            if ( subSelected.includes(el) ) {
+              cnt = cnt + 1;
+            }
+          })
+          if (cnt == 0) {
+            this.sendAction('onChange', true, el, type);
+          }
+          if (cnt == nodesNum) {
+            self.sendAction('onChange', false, el, type);
+          }
+        }
+      });
+    }
+
   }),
 
   nodes: computed('', function(){
