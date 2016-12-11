4a2a7b639410e180b17db0d4abe584738af2521b
Olga Brani <olgabrani78@gmail.com>
2016-12-11 23:38:43 +0200

    ui: Add assistant model

---
+++ ui/app/models/assistant.js
@@ -0,0 +1,11 @@
+import User from 'ui/models/user';
+import {normalizeUser, serializeUser, normalizeUserErrors} from 'ui/utils/common/users';
+import assistantFields from 'ui/mixins/institution-manager';
+
+export default User.extend(assistantFields, {
+  __api__: {
+    normalizeErrors: normalizeUserErrors,
+    normalize: normalizeUser,
+    serialize: serializeUser
+  },
+});
