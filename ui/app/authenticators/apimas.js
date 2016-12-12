bbeb35f7b49b6997d75d1dd59bf2f373e4c04dfe
Olga Brani <olgabrani78@gmail.com>
2016-12-12 12:02:13 +0200

    Revert "ui: Get assistant role from manager_role"
    This reverts commit 356bc9973bba2b9944b6e433b7d5a3f10c06a5e0.

--- ui/app/authenticators/apimas.js
+++ ui/app/authenticators/apimas.js
@@ -19,9 +19,6 @@ export default Token.extend({
       },
     }).then(function(res){
       merge(response, res.user);
-      if (response.manager_role = 'assistant') {
-        response['role'] = 'assistant'
-      }
       return response;
     });
   },
