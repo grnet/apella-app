6e33cd54027d7d82170786d6a5f5ae654b651cc0
Efthymia Bika <ey8ymia@grnet.gr>
2016-12-12 17:22:29 +0200

    Update permission rules

--- apella/permissions/permission_rules.py
+++ apella/permissions/permission_rules.py
@@ -189,7 +189,8 @@ PERMISSION_RULES = [
     ('user-interests', 'list', 'candidate', '*', '*', ''),
     ('user-interests', 'retrieve', 'candidate', '*', 'owned', ''),
     ('user-interests', 'update', 'candidate', '*', 'owned', ''),
-    ('user-interests', 'list', 'helpdeskadmin', '*', '*', ''),
     ('users', 'retrieve', 'professor', '*', '*', ''),
     ('users', 'retrieve', 'candidate', '*', '*', ''),
+    ('users', 'list', 'helpdeskadmin', '*', '*', '')
+
 ]
