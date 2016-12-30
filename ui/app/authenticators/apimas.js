cb961fe910889fcc03790a51ee14f1b022f42b40
Kostas Papadimitriou <kpap@grnet.gr>
2016-12-30 13:19:46 +0200

    ui: Authenticator improvements
    - Refresh user data during session restore
    - Inject pending_role attribute to session data when user is not
      verified. This limits non-verified users from accessing non-profile
      views.

--- ui/app/authenticators/apimas.js
+++ ui/app/authenticators/apimas.js
@@ -6,10 +6,41 @@ const {
   merge, computed
 } = Ember;
 
+function mergeProfileData(sessionData, profileResponse) {
+  let user = profileResponse.user;
+  user.user_id = user.id;
+  delete user.id;
+  delete profileResponse.user;
+  merge(sessionData, user);
+  merge(sessionData, profileResponse);
+
+  // process unverified users as no-role users
+  if (sessionData.hasOwnProperty('is_verified') && sessionData.is_verified === false) {
+    sessionData.pending_role = sessionData.role;
+    delete sessionData.role;
+  }
+  return sessionData;
+}
+
 export default Token.extend({
   init() {
     this._super();
     this.serverTokenEndpoint = ENV.APP.backend_host + '/auth/login/';
+  },
+
+  getUserProfile(token) {
+    return $.ajax({
+      url: `${ENV.APP.backend_host}/auth/me/`,
+      method: 'GET',
+      contentType: 'application/json',
+      headers: {
+        'Authorization': `Token ${token}`
+      },
+    });
+  },
+
+  restore(data) {
+    return this.getUserProfile(data.auth_token).then(mergeProfileData.bind(this, data));
   },
 
   authenticate(credentials, headers) {
@@ -28,24 +59,7 @@ export default Token.extend({
   },
 
   getResponseData(response) {
-    let token = response.auth_token;
-
-    return $.ajax({
-      url: `${ENV.APP.backend_host}/auth/me/`,
-      method: 'GET',
-      contentType: 'application/json',
-      headers: {
-        'Authorization': `Token ${token}`
-      },
-    }).then(function(res){
-      let user = res.user;
-      user.user_id = user.id;
-      delete user.id;
-      delete res.user;
-      merge(response, user);
-      merge(response, res);
-      return response;
-    });
+    return this.getUserProfile(response.auth_token).then(mergeProfileData.bind(this, response));
   },
 
 })
