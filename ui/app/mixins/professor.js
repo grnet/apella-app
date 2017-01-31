a7f9a84ccb67c87c8fa69c822df5830bb7f099b2
Olga Brani <olgabrani@grnet.gr>
2017-01-31 16:54:56 +0200

    ui: Use boolean-display for professor boolean fields

--- ui/app/mixins/professor.js
+++ ui/app/mixins/professor.js
@@ -23,21 +23,21 @@ export default Ember.Mixin.create({
   }),
   rank: DS.attr({type: 'select', choices: CHOICES.RANKS, defaultValue:'Assistant Professor', translate: true}),
   rank_verbose: computeI18NChoice('rank', CHOICES.RANKS),
-  is_foreign: DS.attr({type: 'boolean', defaultValue: false }),
+  is_foreign: DS.attr({type: 'boolean', defaultValue: false, displayComponent: 'boolean-display' }),
   is_foreign_descr: computed('is_foreign', 'locale.i18n', function() {
     let is_foreign = get(this, 'is_foreign');
     return is_foreign ? get(this, 'i18n').t('professor_foreign') : get(this, 'i18n').t('professor_domestic');
   }),
-  speaks_greek: DS.attr({type: 'boolean', defaultValue: true }),
+  speaks_greek: DS.attr({type: 'boolean', defaultValue: true, displayComponent: 'boolean-display' }),
   speaks_greek_verbose: booleanFormat('speaks_greek'),
-  cv_url: DS.attr(),
+  cv_url: DS.attr({displayComponent: 'url-display'}),
   cv: DS.belongsTo('apella-file'),
   cv_professor: DS.belongsTo('apella-file'),
   diplomas: DS.hasMany('apella-file'),
   publications: DS.hasMany('apella-file'),
   fek: DS.attr(),
   discipline_text: DS.attr(),
-  discipline_in_fek: DS.attr({type: 'boolean', defaultValue: true}),
+  discipline_in_fek: DS.attr({type: 'boolean', defaultValue: true, displayComponent: 'boolean-display'}),
   discipline_in_fek_verbose: booleanFormat('discipline_in_fek'),
   active_elections: DS.attr('number'),
 
