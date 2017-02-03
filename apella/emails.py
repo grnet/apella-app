b081040e94a1bffffebac384cbfef916a4b2628b
Efthymia Bika <ey8ymia@grnet.gr>
2017-02-03 14:23:57 +0200

    Set login url for email body

--- apella/emails.py
+++ apella/emails.py
@@ -2,15 +2,20 @@ from django.conf import settings
 from django.core.mail import send_mail
 from django.template.loader import render_to_string
 
+from apella.util import urljoin
+
 
 def send_new_credentials_to_old_users_email(old_users):
+    BASE_URL = settings.BASE_URL or '/'
+    login_url = urljoin(
+        BASE_URL, settings.API_PREFIX, settings.TOKEN_LOGIN_URL)
     for old_user in old_users:
         subject = render_to_string(
             'apella/emails/old_user_new_credentials_subject.txt'). \
             replace('\n', ' ')
         body = render_to_string(
             'apella/emails/old_user_new_credentials_body.txt',
-            {'old_user': old_user, 'baseurl': settings.BASE_URL}
+            {'old_user': old_user, 'login_url': login_url}
         )
         sender = 'no_reply@grnet.gr'
         send_mail(
