6a4c429214bb096180bac4495c4c53f465be5c24
Olga Brani <olgabrani@grnet.gr>
2017-02-08 12:07:54 +0200

    ui-build: Exclude logo images from fingerprinting

--- ui/ember-cli-build.js
+++ ui/ember-cli-build.js
@@ -5,7 +5,7 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
     fingerprint: {
-      exclude: ['**/flag_*.png'],
+      exclude: ['**/flag_*.png', '**/logo_*.png'],
     }
     // Add options here
   });
