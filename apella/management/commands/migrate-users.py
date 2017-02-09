1a8b188a92b8009944d35797dcf60bd33b2ad943
Efthymia Bika <ey8ymia@grnet.gr>
2017-02-09 12:24:10 +0200

    Add command to migrate users from csv file

---
+++ apella/management/commands/migrate-users.py
@@ -0,0 +1,35 @@
+import csv
+
+from django.core.management import BaseCommand, CommandError
+
+from apella.models import OldApellaUserMigrationData
+from apella.migration_functions import migrate_username, migrate_shibboleth_id
+
+
+class Command(BaseCommand):
+    help = "Migrate users from csv"
+
+    def add_arguments(self, parser):
+        parser.add_argument('csv_file')
+
+    def handle(self, *args, **options):
+        csv_file_path = options['csv_file']
+
+        with open(csv_file_path) as f:
+            csv_reader = csv.reader(f)
+
+            csv_iterator = iter(csv_reader)
+            for header in csv_iterator:
+                break
+            else:
+                m = "No csv data"
+                raise CommandError(m)
+
+            for row in csv_iterator:
+                old_user_id = row[0]
+                old_users = OldApellaUserMigrationData.objects.filter(
+                    user_id=old_user_id)
+                if old_users[0].shibboleth_id:
+                    migrate_shibboleth_id(old_users[0].shibboleth_id)
+                else:
+                    migrate_username(old_users[0].username)
