246f8b7b3dd75347980649af98c28991f17e13de
Georgios D. Tsoukalas <gtsouk@grnet.gr>
2017-02-15 15:49:36 +0200

    Fix: only return snapshot versions of candidacy files

--- resources/migration_queries/get_apella_candidacy_files_migrate_data.sql
+++ resources/migration_queries/get_apella_candidacy_files_migrate_data.sql
@@ -4,7 +4,7 @@ select
     c.id candidacy_serial,
     p.id position_serial,
     c.candidate_id candidate_user_id,
-    fh.id file_id,
+    fb.id file_id,
     fh.type as file_type,
     fb.storedfilepath file_path,
     fb.originalfilename original_name,
@@ -17,13 +17,16 @@ from
     positioncandidacies pc,
     candidacy c,
     (   select
-            id header_id,
-            candidacy_id
+            fb.id file_id,
+            cf.candidacy_id candidacy_id
         from
-            candidacyfile
+            candidacyfile cf,
+            filebody fb
+        where
+            fb.header_id = cf.id
         union
         select
-            fb.header_id header_id,
+            fb.id file_id,
             cfb.candidacy_id candidacy_id
         from
             candidacy_filebody cfb,
@@ -37,7 +40,7 @@ where
     pc.position_id = p.id
     and pc.id = c.candidacies_id
     and cf.candidacy_id = c.id
-    and cf.header_id = fh.id
+    and cf.file_id = fb.id
     and fb.header_id = fh.id
     and fh.deleted is false
     and p.id in :positions_to_migrate
