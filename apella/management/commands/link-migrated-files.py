24b023d894550e1c2fe57900e016eece43a8b6ae
Efthymia Bika <ey8ymia@grnet.gr>
2017-02-20 14:53:40 +0200

    Select files with old file path to link

--- apella/management/commands/link-migrated-files.py
+++ apella/management/commands/link-migrated-files.py
@@ -3,11 +3,12 @@ import csv
 from django.core.management import BaseCommand, CommandError
 
 from apella.migration_functions import link_migrated_files
+from apella.models import ApellaFile
 
 
 class Command(BaseCommand):
     help = "Ensure migrated files are hard-linked on disk"
 
     def handle(self, *args, **options):
-        files = ApellaFile.objects.filter(old_file_path=True)
+        files = ApellaFile.objects.exclude(old_file_path='')
         link_migrated_files(files)
