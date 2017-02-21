d6ae0bf416d871414db1587cf80185de3050a561
Georgios D. Tsoukalas <gtsouk@grnet.gr>
2017-02-21 23:58:54 +0200

    Add resume to migrate-users

--- apella/management/commands/migrate-users.py
+++ apella/management/commands/migrate-users.py
@@ -5,7 +5,6 @@ import csv
 
 from django.core.management import BaseCommand, CommandError
 
-from apella.models import OldApellaUserMigrationData
 from apella.migration_functions import migrate_username, migrate_shibboleth_id
 
 
@@ -17,10 +16,20 @@ class Command(BaseCommand):
         parser.add_argument('-p', '--parallel',
                             dest='parallel', type=str, default='1/1',
                             help="Execute (part of) user migrations. Default: 1/1")
+        parser.add_argument('-s', '--start-at',
+                            dest='start_at', type=int, default=None,
+                            help=("If specified, start at this row. "
+                                  "Helpful to continue migration "
+                                  "where it was interrupted."))
+        parser.add_argument('-e', '--end-at',
+                            dest='end_at', type=int, default=None,
+                            help=("If specified, end at this row."))
 
     def handle(self, *args, **options):
         csv_file_path = options['csv_file']
         parallel = options['parallel']
+        start_at = options['start_at']
+        end_at = options['end_at']
         import re
         parallel_pattern = '^[1-9][0-9]*/[1-9][0-9]*$'
         if not re.match(parallel_pattern, parallel):
@@ -56,6 +65,20 @@ class Command(BaseCommand):
             if this_part_no == nr_all_parts \
             else (start_line_no + part_size)
 
+        if start_at is None:
+            start_at = start_line_no
+        elif not start_line_no <= start_at < end_line_no:
+            m = "Invalid --start-at: %d <= %d < %d"
+            m %= (start_line_no, start_at, end_line_no)
+            raise CommandError(m)
+
+        if end_at is None:
+            end_at = end_line_no
+        elif not start_line_no < end_at <= end_line_no:
+            m = "Invalid --end-at: %d < %d <= %d"
+            m %= (start_line_no, end_at, end_line_no)
+            raise CommandError(m)
+
         with open(csv_file_path) as f:
             csv_reader = csv.reader(f)
 
@@ -68,11 +91,11 @@ class Command(BaseCommand):
 
             line_no = 0
             for username, shibboleth_id in csv_iterator:
-                if start_line_no <= line_no < end_line_no:
-                    if line_no % 100 == 0:
-                        m = "migrating %d <= %d < %d"
-                        m %= (start_line_no, line_no, end_line_no)
-                        logger.info(m)
+                if start_at <= line_no < end_at:
+                    m = "migrating user %r/%r row %d <= %d < %d of %d"
+                    m %= (username, shibboleth_id,
+                          start_line_no, line_no, end_line_no, nr_lines)
+                    logger.info(m)
                     if shibboleth_id:
                         migrate_shibboleth_id(
                             apella2_shibboleth_id=None,
