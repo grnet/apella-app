711bc3c44fcbd5ee29cc84a12bd72eb0bcb75952
Athina Bekakou <athinabek@grnet.gr>
2017-03-01 17:06:50 +0200

    ui: Change sorting attributes of candidacies
    Sort by position.id instead of position.code in order to sort the candidacies
    like their codes are numbers not strings.

--- ui/app/gen/candidacy.js
+++ ui/app/gen/candidacy.js
@@ -127,7 +127,7 @@ let FS = {
     POSITION_FIELDSET,
     CANDIDATE_FIELDSET,
   ],
-  list:  ['position.code', 'position.title', 'position.department.institution.title_current',
+  list:  [field('position.code', {dataKey: 'position__id'}), 'position.title', 'position.department.institution.title_current',
           'position.department.title_current',
           'position.state_calc_verbose', field('state_verbose', {label: 'candidacy.state'})],
   list_with_user_id:  ['position.code', field('candidate.id', {label: 'candidate.user_id.label'}), 'position.title', 'position.department.institution.title_current',
