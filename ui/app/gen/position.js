f515a4367b31bf36e8088baa05a02d06f697fbb4
Athina Bekakou <athinabek@grnet.gr>
2017-03-03 13:22:49 +0200

    ui: Gather abilityStates of positions and positions recent

--- ui/app/gen/position.js
+++ ui/app/gen/position.js
@@ -9,6 +9,7 @@ import {
   goToDetails, applyCandidacy, positionActions
 } from 'ui/utils/common/actions';
 import {pick_edit_fs, pick_details_fs, pick_create_fs} from 'ui/lib/position/pick_fs_functions';
+import {abilityStates} from 'ui/lib/position/abilityStates';
 
 /*
  * This gen is used for displaying:
@@ -32,55 +33,7 @@ export default ApellaGen.extend({
   auth: true,
   path: 'positions',
 
-  abilityStates: {
-    is_latest: computed('model.is_latest', function(){
-       return get(this, 'model.is_latest');
-    }),
-    can_create: computed('user.can_create_positions', 'role', function() {
-      let role = get(this, 'role');
-      let can_create = get(this, 'user.can_create_positions');
-      if (role === 'assistant') {
-        return can_create;
-      }
-      return true
-    }),
-    'open': computed('model.state', 'model.ends_at', 'is_latest',  function() {
-      return get(this, 'model.state') === 'open' &&
-        moment(get(this, 'model.ends_at')).isBefore(new Date()) &&
-        get(this, 'is_latest')
-    }),
-    electing: computed('state', 'can_create', 'is_latest', function() {
-      return get(this, 'model.state') === 'electing' &&
-        get(this, 'is_latest') &&
-        get(this, 'can_create');
-    }),
-    before_open: computed('model.is_posted', 'is_latest', 'can_create', function(){
-      return get(this, 'model.is_posted') &&
-        get(this, 'is_latest') &&
-        get(this, 'can_create');
-    }),
-    after_closed: computed('model.is_closed', 'is_latest', 'can_create', function(){
-      return get(this, 'model.is_closed') &&
-        get(this, 'is_latest') &&
-        get(this, 'can_create');
-    }),
-    revoked: computed('model.state', 'is_latest', 'can_create', function(){
-      return get(this, 'model.state') === 'revoked' &&
-        get(this, 'is_latest') &&
-        get(this, 'can_create');
-    }),
-    owned: computed('model.author.user_id', 'user.user_id', 'role',  function() {
-      let is_author = this.get('model.author.user_id') == get(this, 'user.user_id');
-      let is_manager = get(this, 'role') == 'institutionmanager';
-      return is_manager || is_author;
-    }),
-    owned_by_assistant: computed('role', 'user.can_create_positions', 'is_latest', function(){
-      let is_assistant = get(this, 'role') == 'assistant';
-      let can_create = get(this, 'user.can_create_positions');
-      let is_latest = get(this, 'is_latest');
-      return is_assistant && can_create && is_latest;
-    })
-  },
+  abilityStates: abilityStates,
 
   common: {
     validators: {
