029adec81d668109bdcdc1d6f364692a49318af8
Efthymia Bika <ey8ymia@grnet.gr>
2017-03-04 14:19:39 +0200

    Fix typo

--- apella/emails.py
+++ apella/emails.py
@@ -407,7 +407,7 @@ def send_emails_members_change(position, type, old_members, new_members):
             for removed_reg in removed_regular:
                 send_user_email(
                     removed_reg.user,
-                    'apella /emails/position_set_electors_subject.txt',
+                    'apella/emails/position_set_electors_subject.txt',
                     'apella/emails/position_remove_elector_to_regular_body.txt',
                     {'position': position})
             for removed_irreg in removed_irregular:
