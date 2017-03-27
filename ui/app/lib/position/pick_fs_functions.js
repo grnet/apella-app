4110f5538afb68dc431caba1edfd738fd4ba3a23
Olga Brani <olgabrani@grnet.gr>
2017-03-27 15:47:41 +0300

    ui: Hide position's election fieldset

--- ui/app/lib/position/pick_fs_functions.js
+++ ui/app/lib/position/pick_fs_functions.js
@@ -41,7 +41,7 @@ const pick_edit_fs = function() {
   }
 
   if (state === 'electing') {
-    res = res.concat(fs.electors, fs.electors_regular, fs.electors_substitite, fs.committee, fs.election);
+    res = res.concat(fs.electors, fs.electors_regular, fs.electors_substitite, fs.committee/*, fs.election*/);
   }
 
   return res;
@@ -70,10 +70,10 @@ const pick_details_fs_by_state = function(fs, state, before_open, head, display_
   // in all other states
   else {
     if (display_candidacies) {
-      res = head.concat(fs.candidacies, fs.electors, fs.electors_regular, fs.electors_substitite, fs.committee, fs.election);
+      res = head.concat(fs.candidacies, fs.electors, fs.electors_regular, fs.electors_substitite, fs.committee/*, fs.election*/);
     }
     else {
-      res = head.concat(fs.electors, fs.electors_regular, fs.electors_substitite, fs.committee, fs.election);
+      res = head.concat(fs.electors, fs.electors_regular, fs.electors_substitite, fs.committee/*, fs.election*/);
     }
   }
   return res.concat(tail);
