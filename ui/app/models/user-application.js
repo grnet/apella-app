a2554a7f34cf3918686805c2f49ff7d65eed8f57
Olga Brani <olgabrani@grnet.gr>
2017-04-05 16:08:21 +0300

    ui: Add more fields to user-application model
    Add position_id and can_apply fields

--- ui/app/models/user-application.js
+++ ui/app/models/user-application.js
@@ -2,7 +2,13 @@ import DS from 'ember-data';
 import ENV from 'ui/config/environment';
 import {computeDateTimeFormat, computeI18NChoice, prefixSelect} from 'ui/lib/common';
 
-const CHOICES = ENV.APP.resources;
+const {
+  computed,
+  get
+} = Ember,
+  CHOICES = ENV.APP.resources;
+
+
 
 let application_types = prefixSelect(CHOICES.APPLICATION_TYPES, 'user_application.');
 let application_states = prefixSelect(CHOICES.APPLICATION_STATES, 'user_application.');
@@ -10,8 +16,14 @@ let application_states = prefixSelect(CHOICES.APPLICATION_STATES, 'user_applicat
 export default DS.Model.extend({
   app_type: DS.attr({type: 'select', choices: application_types, defaultValue: 'tenure'}),
   app_type_verbose: computeI18NChoice('app_type', application_types),
+  can_accept_candidacies: DS.attr(),
   created_at: DS.attr('date'),
   created_at_format: computeDateTimeFormat('created_at'),
+  position_id: DS.attr(),
+  position_id_format: computed('position_id', function(){
+    let position_id = get(this, 'position_id');
+    return position_id > 0 ? position_id: '-';
+  }),
   state: DS.attr({type: 'select', choices: application_states, defaultValue: 'pending'}),
   state_verbose: computeI18NChoice('state', application_states),
   updated_at: DS.attr('date'),
