51c843555ac8192dbba25ed5befbda9baf077c73
Athina Bekakou <athinabek@grnet.gr>
2017-04-06 14:56:07 +0300

    ui: Change institution filter of institution managers

--- ui/app/gen/institution-manager.js
+++ ui/app/gen/institution-manager.js
@@ -64,7 +64,24 @@ export default ApellaGen.extend({
       active: true,
       serverSide: true,
       meta: {
-        fields: [field('institution', {autocomplete: true}), 'is_verified', 'is_rejected', 'verification_pending', field('no_verification_request', { type: 'boolean' })]
+        fields: [
+          field('institution', {
+            query: function(select, store, field, params) {
+              let locale = get(select, 'i18n.locale'),
+                ordering_param = `title__${locale}`;
+              params = params || {};
+              params.ordering = ordering_param;
+              params.category = 'Institution';
+
+              return store.query('institution', params);
+            },
+            autocomplete: true
+          }),
+          'is_verified',
+          'is_rejected',
+          'verification_pending',
+          field('no_verification_request', { type: 'boolean' })
+        ]
       },
       search: true,
       searchFields: ['user_id', 'email', 'username', 'first_name', 'last_name']
