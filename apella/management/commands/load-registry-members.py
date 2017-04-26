ebe73b8edd0cb4322f2e7eedaa8e73723a075abd
Efthymia Bika <ey8ymia@grnet.gr>
2017-04-26 11:56:53 +0300

    Add old-ids argument to load registry members command

--- apella/management/commands/load-registry-members.py
+++ apella/management/commands/load-registry-members.py
@@ -18,15 +18,19 @@ class Command(ApellaCommand):
         parser.add_argument('--department-id',
                             dest='department_id',
                             help='Department id')
-
         parser.add_argument('--type',
                             dest='registry_type',
                             help='Registry type: internal, external')
+        parser.add_argument('--old-ids',
+                            dest='old_ids',
+                            default=False,
+                            help='csv file contains old user ids')
 
     def handle(self, *args, **options):
         department_id = options['department_id']
         registry_type = options['registry_type']
         csv_file_path = options['csv_file']
+        old_ids = options['old_ids']
 
         types = [x[0] for x in common.REGISTRY_TYPES]
         if registry_type not in types:
@@ -46,14 +50,26 @@ class Command(ApellaCommand):
             for user_id in csv_iterator:
                 uid = int(user_id[0])
                 try:
-                    u = ApellaUser.objects.get(id=uid)
-                    users.append(u.professor)
+                    if not old_ids:
+                        u = ApellaUser.objects.get(id=uid)
+                    else:
+                        u = ApellaUser.objects.get(old_user_id=uid)
                 except ApellaUser.DoesNotExist:
-                    raise CommandError(
+                    self.stdout.write(
                         "ApellaUser does not exist, user_id : %r" % uid)
+                    continue
+                try:
+                    p = u.professor
+                    if not p.is_verified:
+                        self.stdout.write(
+                            "Professor profile unverified, user_id : %r" %
+                            uid)
+                        continue
+                    users.append(p)
                 except Professor.DoesNotExist:
-                    raise CommandError(
+                    self.stdout.write(
                         "Professor does not exist, user_id : %r" % uid)
+                    continue
 
         try:
             registry = Registry.objects.get(
@@ -67,7 +83,8 @@ class Command(ApellaCommand):
             if u not in registry.members.all():
                 registry.members.add(u)
                 new_members.append(u)
-                self.stdout.write('adding user %r to registry %r' %
+                self.stdout.write(
+                    'adding user %r to registry %r' %
                     (u.user.id, registry.id))
         registry.save()
         send_registry_emails(new_members, department)
