d47e8c4049a435cba12a8b9a835f05b9c2c72b92
Efthymia Bika <ey8ymia@grnet.gr>
2017-05-25 17:38:11 +0300

    Permit accept_terms action

--- apella/permissions/permissions.py
+++ apella/permissions/permissions.py
@@ -3,6 +3,8 @@ from rest_framework.permissions import BasePermission
 
 class TermsPermission(BasePermission):
     def has_permission(self, request, view):
+        if view.action == 'accept_terms':
+            return True
         user = request.user
         if not hasattr(user, 'has_accepted_terms'):
             return True
