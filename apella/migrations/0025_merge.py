710edd49c87b7941ba20246d801a03a2df28fa6e
Efthymia Bika <ey8ymia@grnet.gr>
2017-05-31 15:34:35 +0300

    Merge migrations

---
+++ apella/migrations/0025_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('apella', '0024_auto_20170518_0908'),
+        ('apella', '0024_auto_20170531_0830'),
+    ]
+
+    operations = [
+    ]
