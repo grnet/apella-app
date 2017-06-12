fd164c7e63b197c24c90b8e1b07bf1e72102cd8f
Kostas Papadimitriou <kpap@grnet.gr>
2017-06-12 17:05:55 +0300

    ui: Include department filter in registries list view
    Department filter is editable once an institution is already selected.
    The available choices are computed based on the selected institution.

--- ui/app/gen/registry.js
+++ ui/app/gen/registry.js
@@ -302,6 +302,28 @@ export default ApellaGen.extend({
                 return store.query('institution', params);
               }
             }),
+          field('department',
+            {
+              autocomplete: true,
+              label: 'department.label',
+              type: 'model',
+              displayAttr: 'title_current',
+              modelName: 'department',
+              disabled: computed('model.changeset.institution', function() {
+                return !this.get('model.changeset.institution');
+              }),
+              dataKey: 'department',
+              query: computed('model.changeset.institution', function() {
+                let inst = this.get('model.changeset.institution.id');
+                return function(select, store, field, params) {
+                  let locale = select.get('i18n.locale');
+                  params = params || {};
+                  params.ordering = `title_${locale}`;
+                  params.institution = inst;
+                  return store.query('department', params);
+                }
+              })
+            }),
           'type',
           field('id', {type: 'string'}),
         ]
