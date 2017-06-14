e568e17d1713b9eb4139ce385c167b1337036100
Olga Brani <olgabrani@grnet.gr>
2017-06-14 12:05:17 +0300

    ui: Optional plural dataKey for departmentInstitutionFilterField
    API filtering by department can be department for belongsTo relations
    and departments for hasMany relations

--- ui/app/utils/common/fields.js
+++ ui/app/utils/common/fields.js
@@ -12,30 +12,32 @@ function disable_field(el) {
   })
 }
 
-let departmentInstitutionFilterField =  field(
-  'department',
-  {
-    autocomplete: true,
-    label: 'department.label',
-    type: 'model',
-    displayAttr: 'title_current',
-    modelName: 'department',
-    disabled: computed('model.changeset.institution', function() {
-      return !this.get('model.changeset.institution');
-    }),
-    dataKey: 'department',
-    query: computed('model.changeset.institution', function() {
-      let inst = this.get('model.changeset.institution.id');
-      return function(select, store, field, params) {
-        let locale = select.get('i18n.locale');
-        params = params || {};
-        params.ordering = `title_${locale}`;
-        params.institution = inst;
-        return store.query('department', params);
-      }
-    })
-  }
-);
-
+function departmentInstitutionFilterField(plural=false) {
+  let dataKey = plural? 'departments': 'department';
+  return field(
+    'department',
+    {
+      autocomplete: true,
+      label: 'department.label',
+      type: 'model',
+      displayAttr: 'title_current',
+      modelName: 'department',
+      disabled: computed('model.changeset.institution', function() {
+        return !this.get('model.changeset.institution');
+      }),
+      dataKey: dataKey,
+      query: computed('model.changeset.institution', function() {
+        let inst = this.get('model.changeset.institution.id');
+        return function(select, store, field, params) {
+          let locale = select.get('i18n.locale');
+          params = params || {};
+          params.ordering = `title_${locale}`;
+          params.institution = inst;
+          return store.query('department', params);
+        }
+      })
+    }
+  );
+}
 
 export {disable_field, departmentInstitutionFilterField}
