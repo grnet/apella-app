3b64f7c64bde3c07292924b28e8332ff9c0a9406
Efthymia Bika <ey8ymia@grnet.gr>
2017-07-03 14:57:04 +0300

    Log verify/reject user actions

--- apella/views/mixins.py
+++ apella/views/mixins.py
@@ -474,6 +474,9 @@ class CandidateProfile(object):
                     candidate_user.user.can_set_academic = True
                     candidate_user.user.save()
             candidate_user.save()
+            logger.info(
+                'user %s verified profile %r' %
+                (request.user.username, candidate_user.id))
             send_user_email(
                 candidate_user.user,
                 'apella/emails/user_verified_profile_subject.txt',
@@ -491,6 +494,9 @@ class CandidateProfile(object):
                 reason = request.data['rejected_reason']
             auth_hooks.reject_user(candidate_user, reason=reason)
             candidate_user.save()
+            logger.info(
+                'user %s rejected profile %r' %
+                (request.user.username, candidate_user.id))
             send_user_email(
                 candidate_user.user,
                 'apella/emails/user_rejected_profile_subject.txt',
