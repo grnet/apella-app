ef6901c3704a4042be5236362be96d66a504c33c
Efthymia Bika <ey8ymia@grnet.gr>
2017-07-26 17:24:21 +0300

    Map different issue types

--- apella/jira_wrapper.py
+++ apella/jira_wrapper.py
@@ -4,15 +4,24 @@ from jira import JIRA
 
 from django.conf import settings
 
+ISSUE_TYPES = {
+    "complaint": "Παράπονα",
+    "error": "Πρόβλημα",
+    "general_information": "Γενικές πληροφορίες",
+    "account_modification": "Μεταβολή στοιχείων",
+    "registration": "Εγγραφή/Πιστοποίηση",
+    "login": "Θέματα πρόσβασης"
+}
+
 
 def create_issue(jira_issue):
     jira = JIRA(
-        options = settings.JIRA_OPTIONS, basic_auth=settings.JIRA_LOGIN)
+        options=settings.JIRA_OPTIONS, basic_auth=settings.JIRA_LOGIN)
     issue = {
         'project': settings.JIRA_PROJECT,
         'summary': jira_issue.title,
         'description': jira_issue.description,
-        'issuetype': {'name' : 'Πρόβλημα'},
+        'issuetype': {'name': ISSUE_TYPES.get(jira_issue.issue_type)},
         'customfield_12350': jira_issue.reporter.username,
         'customfield_12552': jira_issue.user.username,
         'customfield_12550': jira_issue.user.email
