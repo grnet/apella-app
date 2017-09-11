83bc3476b230a36f5bc7505c3acb0c06a83634f8
Efthymia Bika <ey8ymia@grnet.gr>
2017-09-11 13:59:37 +0300

    Update jira fields

--- apella/jira_wrapper.py
+++ apella/jira_wrapper.py
@@ -20,18 +20,21 @@ logger = logging.getLogger(__name__)
 def create_issue(jira_issue):
     jira = JIRA(
         options=settings.JIRA_OPTIONS, basic_auth=settings.JIRA_LOGIN)
-    reporter_name = jira_issue.reporter.first_name.el + " " + \
-        jira_issue.reporter.last_name.el
+    user_fullname = jira_issue.user.first_name.el + " " + \
+        jira_issue.user.last_name.el
     issue = {
         'project': settings.JIRA_PROJECT,
         'labels': [settings.JIRA_LABEL],
         'summary': jira_issue.title,
         'description': jira_issue.description,
         'issuetype': {'name': ISSUE_TYPES.get(jira_issue.issue_type)},
-        'customfield_12552': reporter_name,
-        'customfield_12350': jira_issue.user.username,
-        'customfield_12550': jira_issue.user.email
+        'customfield_12350': user_fullname,
+        'customfield_12550': jira_issue.user.email,
+        'customfield_12553': jira_issue.user.mobile_phone_number
     }
+    if jira_issue.reporter.is_helpdesk():
+        issue.update({'customfield_12751': jira_issue.reporter.username})
+
     created_issue = jira.create_issue(issue)
     logger.info("created jira issue %s" % created_issue.key)
 
