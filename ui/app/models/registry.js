b9aaf377e8bd7984bed0b9110b0987a2c4f455ce
Kostas Papadimitriou <kpap@grnet.gr>
2018-03-01 14:12:52 +0200

    Revert f01e6322
    as it breaks registry members remove action

--- ui/app/models/registry.js
+++ ui/app/models/registry.js
@@ -13,7 +13,7 @@ export default DS.Model.extend({
     normalize: function(hash, serializer) {
       delete hash.members;
       hash.links = {
-        members: ENV.APP.backend_host + '/registries/' + hash.id + '/members/?ordering=user__last_name__en&limit=1'
+        members: ENV.APP.backend_host + '/registries/' + hash.id + '/members/?ordering=user__last_name__en'
       };
       return hash;
     }
