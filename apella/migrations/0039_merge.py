9166f476e3d39229312f0b83015bac23e9d05084
Efthymia Bika <ey8ymia@grnet.gr>
2018-07-24 12:36:27 +0300

    Merge migrations

---
+++ apella/migrations/0039_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('apella', '0038_remove_registry_members'),
+        ('apella', '0038_auto_20180611_0826'),
+    ]
+
+    operations = [
+    ]
